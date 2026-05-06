2cc91fbcc355aac56a6bff8a373104d8a352abc8
Merlin Ran <merlinran@gmail.com>
2026-05-06 09:30:05 -0400

    Add voice control service module

---
+++ modules/services/voice/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+}
+
+android {
+    namespace = "au.com.shiftyjelly.pocketcasts.voice"
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+dependencies {
+    ksp(libs.dagger.hilt.compiler)
+    ksp(libs.hilt.compiler)
+
+    api(libs.dagger.hilt.android)
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.coroutines.core)
+    implementation(libs.timber)
+
+    implementation(projects.modules.services.analytics)
+    implementation(projects.modules.services.coroutines)
+    implementation(projects.modules.services.localization)
+    implementation(projects.modules.services.preferences)
+    implementation(projects.modules.services.repositories)
+    implementation(projects.modules.services.utils)
+
+    testImplementation(libs.coroutines.test)
+    testImplementation(libs.junit)
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.mockito.kotlin)
+    testImplementation(libs.robolectric)
+    testImplementation(libs.turbine)
+    testImplementation(projects.modules.services.sharedtest)
+}

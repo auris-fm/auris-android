4dcd2dd5e507e42fb97b532fd5dbbcba9a14b670
Merlin Ran <merlinran@gmail.com>
2026-05-06 19:31:08 -0400

    Test paused playback voice context

--- modules/services/voice/src/test/kotlin/au/com/shiftyjelly/pocketcasts/voice/playback/PlaybackContextRuleTest.kt
+++ modules/services/voice/src/test/kotlin/au/com/shiftyjelly/pocketcasts/voice/playback/PlaybackContextRuleTest.kt
@@ -1,11 +1,38 @@
+@file:OptIn(kotlinx.coroutines.ExperimentalCoroutinesApi::class)
+
 package au.com.shiftyjelly.pocketcasts.voice.playback
 
+import au.com.shiftyjelly.pocketcasts.repositories.playback.PlaybackManager
+import au.com.shiftyjelly.pocketcasts.repositories.playback.PlaybackState
 import au.com.shiftyjelly.pocketcasts.voice.gate.VoiceControlRuleState
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.test.runCurrent
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
 
 class PlaybackContextRuleTest {
+    @Test
+    fun `paused playback with current episode has active playback context`() = runTest {
+        val playbackState = MutableStateFlow(
+            PlaybackState(
+                state = PlaybackState.State.PAUSED,
+                episodeUuid = "episode-id",
+            ),
+        )
+        val playbackManager: PlaybackManager = mock {
+            on { playbackStateFlow } doReturn playbackState as Flow<PlaybackState>
+        }
+
+        val monitor = PlaybackContextMonitor(playbackManager, backgroundScope)
+        runCurrent()
+
+        assertEquals(PlaybackContext.Active(currentEpisodeUuid = "episode-id"), monitor.context.value)
+    }
+
     @Test
     fun `current episode allows listening even when paused`() {
         val rule = PlaybackContextRule(MutableStateFlow(PlaybackContext.Active(currentEpisodeUuid = "episode-id")))

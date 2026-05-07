bba50868475402241cd0534ccd8fddb8fbc4d8bd
Merlin Ran <merlinran@gmail.com>
2026-05-07 00:21:48 -0400

    Wire voice service controller

---
+++ modules/services/voice/src/main/kotlin/au/com/shiftyjelly/pocketcasts/voice/service/VoiceControlServiceController.kt
@@ -0,0 +1,21 @@
+package au.com.shiftyjelly.pocketcasts.voice.service
+
+import android.content.Context
+import android.content.Intent
+import androidx.core.content.ContextCompat
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class VoiceControlServiceController @Inject constructor(
+    @ApplicationContext private val context: Context,
+) {
+    fun start() {
+        ContextCompat.startForegroundService(context, Intent(context, VoiceControlService::class.java))
+    }
+
+    fun stop() {
+        context.stopService(Intent(context, VoiceControlService::class.java))
+    }
+}

53c6cf1467209fdbf7a6df4bcdf5827a93adbd68
Merlin Ran <merlinran@gmail.com>
2026-05-12 07:47:45 -0400

    Add SourceView.VOICE_COMMANDS for voice-triggered action analytics

--- modules/services/analytics/src/main/java/au/com/shiftyjelly/pocketcasts/analytics/SourceView.kt
+++ modules/services/analytics/src/main/java/au/com/shiftyjelly/pocketcasts/analytics/SourceView.kt
@@ -234,6 +234,10 @@ enum class SourceView(
         key = "widget_player_small",
         analyticsValue = SourceViewType.WidgetPlayerSmall,
     ),
+    VOICE_COMMANDS(
+        key = "voice_commands",
+        analyticsValue = SourceViewType.Unknown,
+    ),
     ;
 
     companion object {
